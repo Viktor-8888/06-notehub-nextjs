afd1882da8c4ba8e4ac475036b9130626a3b068f
Viktor_Shcherbyna <ahmatov@ukr.net>
2026-07-08 22:55:36 +0300

    edited global css and mailto

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,10 +5,9 @@ const Footer = () => {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: your name</p>
+          <p>Developer: Viktor Shcherbyna</p>
           <p>
-            Contact us: {''}
-            <a href="mailto:student@notehub.app">student@notehub.app</a>
+            Contact us: <a href="mailto:77157@ukr.net">student@notehub.app</a>
           </p>
         </div>
       </div>
